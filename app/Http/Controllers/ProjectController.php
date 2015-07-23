37a120d9c3cd600b5bebc1d5c05e2ecb29bd66b5
Cesar <cesarxxe@gmail.com>
2015-07-23 20:21:08 -0300

    Fase 2

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -49,7 +49,7 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        return $this->repository->find($id);
+        return $this->repository->with(['user', 'client'])->find($id);
     }
 
     /**
